65efaaf614192e518ae14ba9181933f5052123cb
Noela-Murugi <murugin254@gmail.com>
2022-08-28 11:07:59 +0300

    function to fetch beer details

--- src/index.js
+++ src/index.js
@@ -9,4 +9,15 @@ function getBeerNameById(){
     })
 }
 
+function getBeerDetails(beerId){
+    fetch(`http://localhost:3000/beers/${beerId}`).then(resp=>resp.json()).then(beer=>{
+        console.log(beer);
+        document.getElementById('beer-name').innerHTML = beer.name;
+        document.getElementById('beer-image').src = beer.image_url;
+        document.getElementById('beer-description').innerHTML = beer.description;
+        document.getElementById('review-list').innerHTML = beer.reviews.map(review=>`<li>${review}</li>`).join('');
+    });
+}
+
+
 
