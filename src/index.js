4fc61ab4a2009e11494be645024fad767e33c57d
Noela-Murugi <murugin254@gmail.com>
2022-08-28 11:07:06 +0300

    function to fetch beer name from api by id

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 //Code goes here
 const apiLocalHost = "http://localhost:3000";
 
+function getBeerNameById(){
+    fetch('http://localhost:3000/beers').then(resp=>resp.json()).then(beers=>{
+        document.getElementById('beer-list').innerHTML = beers
+        .map(beer=>`<li onClick="getBeerDetails(${beer.id})">${beer.name}</li>`)
+        .join('');
+    })
+}
+
+
